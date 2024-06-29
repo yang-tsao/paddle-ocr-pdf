127561986bbc9def32cb927ffa778aa20dbbd656
yang-tsao <caoyang2005@outlook.com>
2024-06-29 14:22:07 +0800

    Update pocr.py
    Add support to rotated PDFs.

--- pocr.py
+++ pocr.py
@@ -1,19 +1,36 @@
-#!/usr/bin/python
-import fitz  # PyMuPDF
+#!/usr/bin/env python3
+import fitz
 import cv2
 import numpy as np
 import tqdm
+import sys
 from paddleocr import PaddleOCR
 import logging
 import argparse
+import pathlib
+import io
 
 logging.disable(logging.DEBUG)
 
 
+def im2stream(im: np.ndarray):
+    _, buffer = cv2.imencode(".bmp", im)
+    bio = io.BytesIO(buffer)
+    return bio
+
+
 def process_pdf(input_pdf_path, output_pdf_path):
     pdf_doc = fitz.open(input_pdf_path)
+    img = fitz.open()
+    if args.pure:
+        pure = fitz.open()
     ocr = PaddleOCR(use_angle_cls=True, lang="ch", use_gpu=True)
-    for page_number in tqdm.tqdm(range(pdf_doc.page_count)):
+    for page_number in tqdm.tqdm(
+        range(
+            pdf_doc.page_count
+            # 10
+        )
+    ):
         page = pdf_doc.load_page(page_number)
         page.add_redact_annot(page.rect)
         page.apply_redactions(images=fitz.PDF_REDACT_IMAGE_NONE)
@@ -27,32 +44,46 @@ def process_pdf(input_pdf_path, output_pdf_path):
             ),
             cv2.COLOR_RGB2BGR,
         )
+        cim = cv2.rotate(cim, (page.rotation // 90 - 1) % 3)
+        cv2.imshow(sys.argv[0], cim)
+        cv2.waitKey(1)
         text = ocr.ocr(cim)
-
-        def conv(C):
-            C[0] /= cim.shape[1]
-            C[1] /= cim.shape[0]
-            C[0] *= page.rect.x1
-            C[1] *= page.rect.y1
-            return C
-
-        fn = "china-s"
         if not text[0]:
             continue
+        img_page = img.new_page(width=cim.shape[1], height=cim.shape[0])
+        img_page.insert_image(img_page.rect, stream=im2stream(cim))
+        if args.pure:
+            new_page = pure.new_page(width=cim.shape[1], height=cim.shape[0])
         for o in text[0]:
-            R = fitz.Rect(conv(o[0][0]), conv(o[0][2]))
+            R = fitz.Rect(o[0][0], o[0][2])
             word = o[1][0]
             if o[1][1] < 0.9:
                 continue
+            fn = "helv" if word.isascii() else "china-s"
             fs = R.width / fitz.get_text_length(word, fn, 1)
-            page.insert_text(
+            img_page.insert_text(
                 (R.x0, R.y1),
                 word,
                 fontname=fn,
                 fontsize=fs,
                 render_mode=3,
             )
-    pdf_doc.save(output_pdf_path, garbage=4, deflate=True)
+            if args.pure:
+                new_page.insert_text(
+                    (R.x0, R.y1),
+                    word,
+                    fontname=fn,
+                    fontsize=fs,
+                    render_mode=0,
+                )
+    if args.pure:
+        pure.set_page_labels(pdf_doc.get_page_labels())
+        pure.save(
+            pathlib.Path(output_pdf_path).stem + "-pure.pdf", garbage=4, deflate=True
+        )
+        pure.close()
+    img.save(output_pdf_path, garbage=4, deflate=True)
+    img.close()
     pdf_doc.close()
 
 
@@ -63,5 +94,7 @@ if __name__ == "__main__":
     )
     parser.add_argument("input_file", help="Input PDF file")
     parser.add_argument("output_file", help="Output PDF file")
+    parser.add_argument("-p", "--pure", action="store_true")
     args = parser.parse_args()
+    cv2.namedWindow(sys.argv[0], cv2.WINDOW_NORMAL)
     process_pdf(args.input_file, args.output_file)
